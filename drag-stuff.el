84cb38477df336a353f8ff28dc7bdef05b52bee2
Johan Andersson <johan.rejeep@gmail.com>
2010-03-13 15:53:55 +0000

    Added empty file.

---
+++ drag-stuff.el
@@ -0,0 +1,47 @@
+;;; drag-stuff.el --- Drag stuff (lines, words, region, etc...) around
+
+;; Copyright (C) 2010 Johan Andersson
+
+;; Author: Johan Andersson <johan.rejeep@gmail.com>
+;; Maintainer: Johan Andersson <johan.rejeep@gmail.com>
+;; Version: 0.0.1
+;; Keywords: speed, convenience
+;; URL: http://github.com/rejeep/drag-stuff
+
+;; This file is NOT part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+
+
+;;; Code:
+
+
+(define-minor-mode drag-stuff-mode
+  ""
+  :init-value nil
+  :lighter " ds"
+  :keymap drag-stuff-mode-map
+  (if drag-stuff-mode
+      
+      ))
+
+(provide 'drag-stuff)
+
+;;; drag-stuff.el ends here
