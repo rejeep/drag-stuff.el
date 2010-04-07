08f4381b044cc15a31614d7bb4152e94ab312c92
Johan Andersson <johan.rejeep@gmail.com>
2010-04-07 23:01:28 +0000

    Use Ecukes and Espuds from submodules.

--- features/support.el
+++ features/support.el
@@ -1,7 +1,12 @@
-(add-to-list 'load-path "~/dev/drag-stuff")
-(require 'drag-stuff)
+(let ((current-directory (file-name-directory load-file-name)))
+  (setq drag-stuff-root-path (expand-file-name ".." current-directory))
+  (setq drag-stuff-util-path (expand-file-name "util" drag-stuff-root-path)))
 
-(add-to-list 'load-path "~/dev/espuds")
+(add-to-list 'load-path drag-stuff-root-path)
+(add-to-list 'load-path (expand-file-name "ecukes" drag-stuff-util-path))
+(add-to-list 'load-path (expand-file-name "espuds" drag-stuff-util-path))
+
+(require 'drag-stuff)
 (require 'espuds)
 
 (Before
